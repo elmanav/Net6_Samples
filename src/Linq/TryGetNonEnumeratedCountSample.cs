e70e77645ff00f8d1fb40e20dc74210e7ed2928f
Elman Aliev <elmanaw25@gmail.com>
2021-11-15 22:58:19 +0300

    Add TryGetNonEnumeratedCount samples.

---
+++ src/Linq/TryGetNonEnumeratedCountSample.cs
@@ -0,0 +1,18 @@
+﻿var databaseItems = new []{"DB_Item1", "DB_Item2"};
+var serviceItems = new []{"Service_Item1", "Service_Item2"};
+var memoryItems = new []{"Memory_Item1", "Memory_Item2"};
+
+WriteCountItems(databaseItems.Concat(serviceItems).Concat(memoryItems));
+WriteCountItems(databaseItems.Where(s => s.Contains("Item2")));
+
+void WriteCountItems(IEnumerable<string> enumerable)
+{
+    if (enumerable.TryGetNonEnumeratedCount(out int count))
+    {
+        Console.WriteLine($"The count is {count}");
+    }
+    else
+    {
+        Console.WriteLine("Could not get a count of items without enumerating the collection");
+    }
+}
