2f16e2645bc0e2e4a3d401f2a9933bf7d583564b
Эльман Алиев <ev.aliev@northis.ru>
2022-06-22 18:54:26 +0300

    Added PeriodicTimer sample.

---
+++ src/Net6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+using var timer = new PeriodicTimer(TimeSpan.FromMilliseconds(1000));
+var sw = Stopwatch.StartNew();
+while (await timer.WaitForNextTickAsync()) // waiting callback 
+    await NewFunction(sw);
+
+async Task NewFunction(Stopwatch stopwatch)
+{
+    await Task.Delay(500); // do some application work 
+    Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds}ms");
+}
